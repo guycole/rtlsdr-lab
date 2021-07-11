42c9b6c4d9b1d13d0358f70de2d9412ba7a81229
Guy Cole <guycole@gmail.com>
2021-07-11 11:58:02 -0700

    start on page 21 of segdsp

---
+++ lab01/segdsp021.py
@@ -0,0 +1,22 @@
+#
+# Title:segdsp021.py
+# Description: historgram, mean and SD
+# Development Environment:OS X 11.0.1/Python 3.8.2
+# Lambda Environment: Python 3.8 runtime
+#
+# page 21 of "Scientist and Engineers Guide to Digital Signal Processing"
+
+class DemoStats:
+
+    def execute(self):
+        print("ryryryr")
+
+if __name__ == "__main__":
+    print("main noted")
+
+    demo = DemoStats()
+    demo.execute()
+
+# ;;; Local Variables: ***
+# ;;; mode:python ***
+# ;;; End: ***
