4c40d881eb09e29c1f4d922f54341e5ee830804d
Guy Cole <guycole@gmail.com>
2024-03-05 23:03:30 -0800

    rtl-adsb test

---
+++ rtl-adsb-lab/decoder1.cxx
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+#define DEFAULT_BUFFER_LENGTH 512
+
+uint8_t bitbuffer[DEFAULT_BUFFER_LENGTH] = {0};
+uint8_t *bitptr;
+
+void decode(const std::string& buffer) {
+    std::cout << buffer << std::endl;
+
+    // buffer looks like "*f6d8c9833540611d6ebdee841425;"
+    // remove the * and ;
+    std::size_t ndx1 = buffer.find("*");
+    std::size_t ndx2 = buffer.find(";");
+    std::string candidate = buffer.substr(ndx1 + 1, ndx2 - ndx1 - 1);
+
+    // convert hex string to binary
+    std::fill(std::begin(bitbuffer), std::end(bitbuffer), 0);
+
+    std::cout << candidate << std::endl;
+    std::cout << candidate.length() << std::endl;
+
+    for (std::size_t ii = 0; ii < candidate.length(); ii++) {
+        std::cout << candidate[ii] << std::endl;
+    }
+
+// 89a30f8d58b982abf57364e2ea61
+// 1000 1 001 
+
+    // 1111 0 110 
+// 2 4 8 16 = 30 // 6
+
+// 801e31
+// 1000 0 000 
+// 16 / 0
+
+// 81a98e
+// 1000 0 001 
+// 16 / 1
+
+// 822152f
+// 1000 0 010
+
+    //string r = s1.substr(3, 2);
+
+    //std::string s = "5f0066";
+    //int num = std::stoi(buffer, 0, 16);
+    //std::cout << num << "\n";
+}
+
+int main(int argc, char* argv[]) {
+    std::string buffer;
+
+    while (std::getline(std::cin, buffer)) {
+        if (buffer.empty()) {
+            break;
+        }
+
+        decode(buffer);
+    }
+
+    return 0;
+}
