b942a977a6a1127af8d73c8dc0e05308ddc1783e
Guy Cole <guycole@gmail.com>
2024-03-12 16:32:39 -0700

    tweak

--- rtl-adsb-lab/mode_s_decoder.cxx
+++ rtl-adsb-lab/mode_s_decoder.cxx
@@ -8,20 +8,20 @@ void mode_s_decoder::converter(std::string raw_buffer) {
 
     // buffer looks like "*f6d8c9833540611d6ebdee841425;"
     // test for "*"" and ";" 
-    std::size_t asterisk = raw_buffer.find("*");
-    if (asterisk == std::string::npos) {
+    std::size_t asterisk_ndx = raw_buffer.find("*");
+    if (asterisk_ndx == std::string::npos) {
         std::cout << "not found x" << std::endl;
         // error
     }
 
-    std::size_t semicolon = raw_buffer.find(";");
-    if (semicolon == std::string::npos) {
+    std::size_t semicolon_ndx = raw_buffer.find(";");
+    if (semicolon_ndx == std::string::npos) {
         std::cout << "not found ;" << std::endl;
         // error
     }
 
     // remove the * and ;
-    std::string temp_string = raw_buffer.substr(asterisk + 1, semicolon - asterisk - 1);
+    std::string temp_string = raw_buffer.substr(asterisk_ndx + 1, semicolon_ndx - asterisk_ndx - 1);
 
     for (int ii = 0, jj=0; ii < temp_string.length(); ii+=2, jj++) { 
         std::string short_buffer = temp_string.substr(ii, 2);
